4ca6f3136cbeac32d99c387f5c68bec6ea74c9f0
Ernesto-Sanchez-J <56661723+Ernesto-Sanchez-J@users.noreply.github.com>
2020-06-24 12:01:19 -0500

    api service updated

--- PunkAPI/src/app/services/api.service.ts
+++ PunkAPI/src/app/services/api.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http/';
+import { IBeer } from '../interfaces/ibeer';
+import { getNumberOfCurrencyDigits } from '@angular/common';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor() { }
+  private BeersURL = 'https://api.punkapi.com/v2/beers';
+  constructor(private httpClient: HttpClient) { }
+}
+
+  async getAll(){
+    return await this.httpClient.get<IBeer[]>(this.BeersURL).toPromise();
+  }
+  async getCount(path){
+    return await this.httpClient.get<IBeer[]>(this.BeersURL + path).toPromise();
+  }
 }
